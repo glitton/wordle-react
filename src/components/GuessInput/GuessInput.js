a6dbf016969e91fed1a69d9eecf70cadb4e0d8ff
Generosa Litton <glcodeworks@gmail.com>
2024-01-07 17:53:47 -0500

    created guess results

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,15 +1,17 @@
 import React from "react";
+function GuessInput({ handleSubmitGuess }) {
+  const [tentativeGuess, setTentativeGuess] = React.useState("");
 
-function GuessInput() {
-  const [guess, setGuess] = React.useState("");
   function handleSubmit(event) {
     event.preventDefault();
-    console.log({ guess });
-    setGuess("");
+    handleSubmitGuess(tentativeGuess);
+    setTentativeGuess("");
   }
+
   return (
     <form onSubmit={handleSubmit} className='guess-input-wrapper'>
       <label htmlFor='guess-input'>Enter guess:</label>
+
       <input
         required
         type='text'
@@ -18,10 +20,10 @@ function GuessInput() {
         maxLength={5}
         pattern='[A-Za-z]{5}'
         title='5 letter word'
-        value={guess}
+        value={tentativeGuess}
         onChange={(event) => {
           const nextGuess = event.target.value.toUpperCase();
-          setGuess(nextGuess);
+          setTentativeGuess(nextGuess);
         }}
       />
     </form>
